79485543dac6438d4d52d21264279194c3ed3e03
Arsenal <arsenaldevnft@gmail.com>
2021-10-08 04:51:34 -0700

    erase

--- src/Features_Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { FeaturesCode } from "./Allcomponents";
-export default function Features_Home() {
-  return (
-    <div>
-      <FeaturesCode />
-    </div>
-  );
-}
