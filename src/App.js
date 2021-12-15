6e3e9c67c5af59b7f11101832323a6a25eba91dc
Arsenal <arsenaldevnft@gmail.com>
2021-12-14 19:37:40 -0800

    Primary push
    Version 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React, { useState } from "react";
-import { Box, Nav, Main, Button, Grommet, Heading, Image, Footer, Paragraph, TextInput, Tabs, Tab } from "grommet";
+import { Box, Nav, Main, Button, Grommet, Heading, Image, Footer, TextInput } from "grommet";
 import { customTheme } from "./theme";
 import Web3 from 'web3';
 import detectEthereumProvider from '@metamask/detect-provider';
 import { master } from './contracts';
 import Timer from "./components/Timer";
+import { Twitter, Tooltip } from 'grommet-icons';
 
 function App() {
   
-  let currentAccount;
+  //let currentAccount;
   let provider;
   let chainid;
 
-  let masterContract;
+  //let masterContract;
+
+  const [masterContract, setMasterContract] = useState();
+  const [currentAccount, setCurrentAccount] = useState();
 
   const [connected, setConnected] = useState(false);
   const [initialized, setinitialized] = useState(false);
   const [disableTimer, setDisableTimer] = useState(true);
   const [minutes, setMinutes] = useState();
   const [seconds, setSeconds] = useState();
+  const [lookup, setLookup] = useState("");
+  const [activenft, setActivenft] = useState("");
+  const [mintingamount, setMintingamount] = useState("");
 
   const Init = async () => {
 
@@ -35,8 +42,8 @@ function App() {
     }
     function startApp(provider) {
       const web3 = new Web3(provider);
-      masterContract = new web3.eth.Contract(master.abi, '0xd1352Bb7A1b3519e53FED5f13658B7FB3B1504dF');
-      // remember to replace abi and addresses
+      setMasterContract( new web3.eth.Contract(master.abi, '0xBb796E285dB07225853a901f29CbFb64408dF00C') );
+      
       if (provider !== window.ethereum) {
         console.error('Do you have multiple wallets installed?');
       }
@@ -76,7 +83,7 @@ function App() {
         setConnected(false)
       }
       else if (accounts[0] !== currentAccount) {
-        currentAccount = accounts[0];
+        setCurrentAccount(accounts[0]);
         setConnected(true);
         timerinit();
       }
@@ -114,55 +121,90 @@ function App() {
     };
   }
 
+  function mintnft(amount) {
+    let price = 100000000000000000;
+    masterContract.methods.mint(amount).send( {from: currentAccount, value: amount*price} );
+  }
+
+  function mintpresalenft() {
+    let price = 100000000000000000;
+    masterContract.methods.mint(1).send( {from: currentAccount, value: price} );
+  }
+
+  function nftfetch(id) {
+    let a = "https://arsenalnft.art/nft/";
+    let b = ".png";
+
+    return a + id + b;
+  }
+
+
+
 
 
 
   return (
     <Grommet theme={customTheme} full>
-      <Box overflow="auto" align="stretch" justify="start" direction="column" background={{ "color": "dark-2" }} fill responsive>
-        <Nav align="center" flex="shrink" justify="between" direction="row" pad={{ "horizontal": "medium" }} overflow="visible" fill="horizontal" height={{"min":"xsmall"}}>
-          <Heading level="3" size="medium" textAlign="center" color="white">
-            Arsenal
+      <Box overflow="auto" align="stretch" justify="start" direction="column" background={{"image":"url('https://arsenalnft.art/backgroundimage.png')","position":"top"}} fill responsive>
+        <Nav align="center" flex="shrink" justify="between" direction="row" pad={{ "horizontal": "medium" }} overflow="visible" fill="horizontal" height={{"min":"medium"}} >
+          <Heading>
           </Heading>
-          {
-            connected ?
-              <Button label="Connected" active={false} disabled={false} primary color="white" />
-              :
-              <Button onClick={() => connect()} label="Connect Wallet" active={false} disabled={false} primary color="white" />
-          }
         </Nav>            
         <Main flex="grow" overflow="visible" align="stretch" justify="start" direction="column" fill="horizontal">
-              <Box align="center" justify="center" direction="column" flex="grow" overflow="hidden" responsive fill="horizontal">
-                {
-                  disableTimer ?
-                    <div></div>
-                    :
-                    <Heading level="2" textAlign="center" color="white" size="medium" margin={{ "bottom": "small" }}>
-                      Able to mint in:
-                    </Heading>
-                }
-                {
-                  disableTimer ?
-                    <Heading level="2" size="large" textAlign="center" color="white" margin={{ "bottom": "medium", "top": "small" }}>
-                      Mint Open!
-                    </Heading>
-                    :
-                    <Timer initialMinute={minutes} initialSeconds={seconds} timerDisabler={setDisableTimer}></ Timer>
-                }
-              </Box>
-              <Box align="center" justify="center" direction="column" flex="grow" overflow="hidden" responsive fill="horizontal">
-                <Heading level="1" size="medium" textAlign="center" color="white" margin="small">
-                  Mint your Arsenal!
+          <Box align="center" justify="center" direction="column" flex="shrink" overflow="hidden" responsive fill="horizontal" pad="large">
+            {
+              connected ?
+                <Button label="Connected" active={false} disabled={false} primary color="#8b572a" />
+                :
+                <Button onClick={() => connect()} label="Connect Wallet" active={false} disabled={false} primary color="#8b572a" />
+            }
+          </Box>
+          <Box align="center" justify="center" direction="column" flex="shrink" overflow="hidden" responsive fill="horizontal" pad="large">
+            {
+              disableTimer ?
+                <div></div>
+                :
+                <Heading level="2" textAlign="center" color="black" size="medium" margin={{ "bottom": "small" }}>
+                  Able to mint in:
                 </Heading>
-              </Box>
-              <Main pad="medium" align="center" justify="center" direction="row-responsive" overflow="hidden" fill="horizontal" flex="grow">
-
-              </Main>
-              <Footer align="center" direction="row" flex justify="center" gap="medium" pad="large">
-                <Heading level="3" size="small" textAlign="center" color="white">
-                  Social Media stuff goes here
+            }
+            {
+              disableTimer ?
+                <Heading level="2" size="large" textAlign="center" color="black" margin={{ "bottom": "medium", "top": "small" }}>
+                  Mint Open!
                 </Heading>
-              </Footer>
+                :
+                <Timer initialMinute={minutes} initialSeconds={seconds} timerDisabler={setDisableTimer}></ Timer>
+            }
+          </Box>
+          <Box align="center" justify="center" direction="column" flex="shrink" overflow="hidden" responsive fill="horizontal">
+            <Heading level="1" size="medium" textAlign="center" color="black" margin="small">
+              View your Arsenal!
+            </Heading>
+          </Box>
+          <Main pad="medium" align="center" justify="center" direction="column" overflow="hidden" fill="horizontal" flex="grow" gap="medium" >
+            <Box align="center" justify="center" direction="column" gap="medium">
+              <Image src={activenft} />
+              <TextInput placeholder="Input NFT # to display" size="large" textAlign="center" type="text" value={lookup} onChange={event => setLookup(event.target.value)} />
+              <Button onClick={() => setActivenft(nftfetch(lookup))} label="Display" size="large" primary color="black" />
+            </Box>
+          </Main>
+          <Main pad="medium" align="center" justify="center" direction="column" overflow="hidden" fill="horizontal" flex="grow" gap="medium" >
+            <Box align="center" justify="center" direction="column" gap="medium">
+              <Heading level="1" size="medium" textAlign="center" color="black" margin="small">
+                Mint your Arsenal!
+              </Heading>
+              <TextInput placeholder="Input amount(limit 10 at a time)" size="large" textAlign="center" type="text" value={mintingamount} onChange={event => setMintingamount(event.target.value)} />
+              <Box align="center" justify="center" direction="row" gap="medium">
+                <Button onClick={() => mintnft(mintingamount)} label="Click here to Mint!" size="large" primary color="black" />
+              </Box>
+              <Button onClick={() => mintpresalenft()} label="Click here if you are in Pre-sale!" size="large" primary color="#8b572a" />
+            </Box>
+          </Main>
+          <Footer align="end" direction="row" flex justify="center" gap="medium" pad="large">
+            <Button primary color="black" icon={<Twitter />} label="" href="https://twitter.com/arsenal_nft" />
+            <Button primary color="black" icon={<Tooltip />} label="" href="https://discord.gg/6XPPFQX48X" />
+          </Footer>
         </Main>
       </Box>
     </Grommet>
--- src/theme.js
+++ src/theme.js
@@ -2,8 +2,14 @@ import { grommet } from "grommet/themes";
 import { deepMerge } from "grommet/utils";
 
 export const customTheme = deepMerge(grommet, {
+  global: {
+    colors: {
+      placeholder: "black"
+    },
+  },
   textInput: {
-    extend: () => `color: #ffffff`
+    extend: () => `color: black`,
+
   },
   tab: {
     active: {color: "black", background: "graph-4" },
